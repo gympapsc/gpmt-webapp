b8ad13d8f3bec6e8eb32fab1aa13bd5941ca12c2
hakrac <hakimrachidi02@gmail.com>
2021-04-06 23:16:38 +0200

    Remove image for simplicity

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,11 @@
 import React from "react"
-import Image from "next/image"
+
 
 
 export default function Index() {
     return (
         <div>
             <h1>Hello World</h1>
-            <Image src="/clouds.jpeg" width="200" height="120" layout="responsive"/>
         </div>
     )
 }
