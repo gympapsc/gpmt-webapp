a65ac21d9fbedfb6a69d006ee2f346071693eb05
hakrac <hakimrachidi02@gmail.com>
2021-07-08 19:29:22 +0200

    Add index page

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Index = () => {
+    return (
+        <div className="font-bold flex flex-col justify-around absolute top-0 bottom-0 right-0 left-0">
+            <h1 className="text-4xl self-center text-center">Gymnasium Papenburg <br/> Miktionstagebuch</h1>
+        </div>
+    )
+}
+
+
+export default Index
