5f5684877a3050d55c63fec2395db9c9c422be5c
hakrac <hakimrachidi02@gmail.com>
2021-08-12 13:34:20 +0200

    Run e2e tests headless

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	launch: {
-		headless: false,
-		slowMo: 10,
-		devtools: true
+		headless: true,
+		// slowMo: 10,
+		// devtools: true
 	},
 	server: {
     	command: 'npm run dev',
