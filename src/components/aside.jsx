25426193945f9603aeabebef1e16ebbfe96b0a2e
hakrac <hakimrachidi02@gmail.com>
2021-09-05 19:07:06 +0200

    Replace cross with chevron in aside drawer

--- src/components/aside.jsx
+++ src/components/aside.jsx
@@ -67,7 +67,7 @@ const Aside = ({ showMenu, setShowMenu }) => {
                     isMobile &&
                     <button onClick={() => setShowMenu(false)} className="w-8 h-8 flex flex-row justify-center items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
                         </svg>
                     </button>
                 }
