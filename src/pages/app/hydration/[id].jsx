041da41d6989e2546fd5543053132d94bfaed3b0
hakrac <hakimrachidi02@gmail.com>
2021-09-23 16:27:53 +0200

    Fix editor reload issue

--- src/pages/app/hydration/[id].jsx
+++ src/pages/app/hydration/[id].jsx
@@ -12,19 +12,13 @@ import TextInput from "../../../components/textInput"
 import { ActivityIndicator } from "../../../components/progressIndicator"
 
 
-const HydrationEdit = () => {
-    let router = useRouter()
-    let slider = useRef(null)
-    let {id} = router.query
-
+const HydrationForm = ({entry, id}) => {
     let dispatch = useDispatch()
-    let entry = useHydration(new Date())
-        .find(d => d._id === id)
-        
-    let updateEntry = e => {
-        entry = e
-    }
-
+    let router = useRouter()
+    let [date, setDate] = useState(entry.date)
+    let [type, setType] = useState(entry.type)
+    let [amount, setAmount] = useState(entry.amount)
+    
     let deleteEntry = e => {
         e.preventDefault()
         dispatch(deleteHydration(id))
@@ -34,82 +28,95 @@ const HydrationEdit = () => {
     let saveEntry = e => {
         e.preventDefault()
         dispatch(updateHydration({
-            ...entry
+            ...entry,
+            date,
+            type,
+            amount
         }))
         router.push("/app")
     }
 
 
     return (
-        <>
-            <Aside />
-            <Shell title={"Trinkeintrag"} className="bg-gray-50" actionButton={
-                <button
-                    onClick={saveEntry} 
-                    className="text-blue-500 self-center inline-flex flex-row transition-colors duration-150 hover:text-blue-600">
-                    <span className="ml-auto font-medium">Speichern</span>
-                </button>
-            }>
-                <div className="flex flex-col px-3 w-full lg:w-3/4 xl:w-2/3 mx-auto space-y-4 h-full">
-                    {
-                        entry ?
-                        <form className="pt-3 space-y-4 flex flex-col justify-between pb-8 h-full">
-                            <div className="space-y-5">
-                                <div className="col-span-full md:mt-8">
-                                    <label className="text-xs text-gray-600 uppercase" htmlFor="amount">Menge</label>
-                                    <div>
-                                        <input
-                                            type="number" 
-                                            className="text-2xl font-semibold inline w-20 bg-transparent text-left"
-                                            value={entry?.amount * 1000}
-                                            min="0"
-                                            max="1000"
-                                            onChange={e => updateEntry({amount: e.target.value / 1000})}
-                                        />
-                                        <span className="text-sm">ml</span>
-                                    </div>
-                                    <input
-                                        type="range"
-                                        className="block w-full"
-                                        id="amount"
-                                        min="0"
-                                        max="1000"
-                                        value={entry?.amount * 1000}
-                                        onChange={e => setAmount(e.target.value / 1000)}
-                                        ref={slider}
-                                        title="Trinkmenge"
-                                        />
-                                </div>
-                                <div className="col-span-full">
-                                    <TextInput label="Trinken" value={entry?.type} onChange={() => {}}/>
-                                </div>
-                                <div className="col-span-full">
-                                    <DateTimeInput label="Datum" value={entry?.date} onChange={() => {}} />
-                                </div>
+        <Shell title={"Trinkeintrag"} className="bg-gray-50" actionButton={
+            <button
+                onClick={saveEntry} 
+                className="text-blue-500 self-center inline-flex flex-row transition-colors duration-150 hover:text-blue-600">
+                <span className="ml-auto font-medium">Speichern</span>
+            </button>
+        }>
+            <div className="flex flex-col px-3 w-full lg:w-3/4 xl:w-2/3 mx-auto space-y-4 h-full">
+                <form className="pt-3 space-y-4 flex flex-col justify-between pb-8 h-full">
+                    <div className="space-y-5">
+                        <div className="col-span-full md:mt-8">
+                            <label className="text-xs text-gray-600 uppercase" htmlFor="amount">Menge</label>
+                            <div>
+                                <input
+                                    type="number" 
+                                    className="text-2xl font-semibold inline w-20 bg-transparent text-left"
+                                    value={amount * 1000}
+                                    min="0"
+                                    onChange={e => setAmount(e.target.value / 1000)}
+                                />
+                                <span className="text-sm">ml</span>
                             </div>
-
-                            <div className="flex flex-row w-full mt-auto">
-                                <button
-                                    onClick={deleteEntry}
-                                    className="py-2 w-full text-center text-white bg-red-600 rounded-lg font-medium transition-colors duration-150 hover:bg-red-700">
-                                    Löschen
-                                </button>
-                            </div>
-                        </form> :
-                        <div className="flex flex-col justify-center items-center h-full w-full">
-                            <ActivityIndicator />
+                            <input
+                                type="range"
+                                className="block w-full"
+                                id="amount"
+                                min="0"
+                                max="1000"
+                                value={amount * 1000}
+                                onChange={e => setAmount(e.target.value / 1000)}
+                                title="Trinkmenge"
+                                />
                         </div>
-                    }
-                </div>
-            </Shell>
-        </>
+                        <div className="col-span-full">
+                            <TextInput label="Trinken" value={type} onChange={setType}/>
+                        </div>
+                        <div className="col-span-full">
+                            <DateTimeInput label="Datum" value={date} onChange={setDate} />
+                        </div>
+                        <input
+                            type="submit"
+                            onClick={saveEntry}
+                            hidden />
+                    </div>
+
+                    <div className="flex flex-row w-full mt-auto">
+                        <button
+                            onClick={deleteEntry}
+                            className="py-2 w-full text-center text-white bg-red-600 rounded-lg font-medium transition-colors duration-150 hover:bg-red-700">
+                            Löschen
+                        </button>
+                    </div>
+                </form> 
+            </div>
+        </Shell>
     )
 }
 
-const SecureHydrationEdit = () => (
-    <Secure>
-        <HydrationEdit></HydrationEdit>
-    </Secure>
-)
+const SecureHydrationEdit = () => {
+    let router = useRouter()
+    let {id} = router.query
+
+    let entry = useHydration(new Date())
+        .find(d => d._id === id)
+    
+    return (
+        <Secure>
+            <Aside />
+            {
+                entry ?
+                <HydrationForm entry={entry} id={id} /> :
+                <Shell title={"Trinkeintrag"} className="bg-gray-50">
+                    <div className="flex flex-col justify-center items-center">
+                        <ActivityIndicator />
+                    </div>
+                </Shell>
+            }
+        </Secure>
+    )
+}
 
 export default SecureHydrationEdit
--- src/pages/app/micturition/[id].jsx
+++ src/pages/app/micturition/[id].jsx
@@ -13,15 +13,10 @@ import {
 } from "../../../actions"
 import { useMicturition } from "../../../hooks"
 
-const Edit = () => {
+const MicturitionForm = ({ entry, id }) => {
     let router = useRouter()
-    let { id } = router.query
     let dispatch = useDispatch()
-
-    let entry = useMicturition(new Date())
-        .find(m => m._id === id)
-    
-    let [date, setDate] = useState(entry?.date || new Date())
+    let [date, setDate] = useState(entry.date)
 
     let deleteEntry = e => {
         e.preventDefault()
@@ -40,39 +35,58 @@ const Edit = () => {
     }
 
     return (
-        <>
-            <Aside />
-            <Shell title="Miktionseintrag" className="bg-gray-50" actionButton={
-                <button
-                    onClick={saveEntry} 
-                    className="text-blue-500 self-center inline-flex flex-row transition-colors duration-150 hover:text-blue-600">
-                    <span className="ml-auto font-medium">Speichern</span>
-                </button>
-            }>
-                <div className="flex flex-col px-3 w-full lg:w-3/4 xl:w-2/3 mx-auto space-y-4 h-full">
-                    <form className="pt-3 space-y-4 flex flex-col justify-between pb-8 h-full">
-                        <div className="col-span-full md:mt-8">
-                            <DateTimeInput value={date} onChange={setDate} label="Datum" />       
-                        </div>
+        <Shell title="Miktionseintrag" className="bg-gray-50" actionButton={
+            <button
+                onClick={saveEntry} 
+                className="text-blue-500 self-center inline-flex flex-row transition-colors duration-150 hover:text-blue-600">
+                <span className="ml-auto font-medium">Speichern</span>
+            </button>
+        }>
+            <div className="flex flex-col px-3 w-full lg:w-3/4 xl:w-2/3 mx-auto space-y-4 h-full">
+                <form className="pt-3 space-y-4 flex flex-col justify-between pb-8 h-full">
+                    <div className="col-span-full md:mt-8">
+                        <DateTimeInput value={date} onChange={setDate} label="Datum" />       
+                    </div>
 
-                        <div className="flex flex-row w-full">
-                            <button
-                                onClick={deleteEntry} 
-                                className="py-2 w-full text-center text-white bg-red-600 rounded-lg font-medium transition-colors duration-150 hover:bg-red-700">
-                                    Löschen
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </Shell>
-        </>
+                    <input
+                        type="submit"
+                        hidden
+                        onClick={saveEntry}
+                    />
+
+
+                    <div className="flex flex-row w-full">
+                        <button
+                            onClick={deleteEntry} 
+                            className="py-2 w-full text-center text-white bg-red-600 rounded-lg font-medium transition-colors duration-150 hover:bg-red-700">
+                                Löschen
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </Shell>
     )
 }
 
 export default function SecureMicturitionEdit() {    
+    let router = useRouter()
+    let {id} = router.query
+
+    let entry = useMicturition(new Date())
+        .find(d => d._id === id)
+    
     return (
         <Secure>
-            <Edit />
+            <Aside />
+            {
+                entry ?
+                <MicturitionForm entry={entry} id={id} /> :
+                <Shell title={"Miktionseintrag"} className="bg-gray-50">
+                    <div className="flex flex-col justify-center items-center">
+                        <ActivityIndicator />
+                    </div>
+                </Shell>
+            }
         </Secure>
     )
 }
